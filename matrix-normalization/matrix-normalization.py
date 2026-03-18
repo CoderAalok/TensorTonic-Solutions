42f7ef612bc92280f54e076ba1fd15e8e34d11d1
Aalok Yadav <yadavaalok565@gmail.com>
2026-03-19 02:46:59 +0545

    Update solution for matrix-normalization

--- matrix-normalization/matrix-normalization.py
+++ matrix-normalization/matrix-normalization.py
@@ -5,25 +5,26 @@ def matrix_normalization(matrix, axis=None, norm_type='l2'):
     Normalize a 2D matrix along specified axis using specified norm.
     """
     # Write code here
-    # edge case
     matrix = np.array(matrix, dtype=float)
-    if matrix.size == 0 or (axis is not None and (axis >= matrix.ndim or axis < -matrix.ndim)) or matrix.ndim > 2:
+    # edge case
+    if matrix.size == 0 or (axis is not None and (axis >= matrix.ndim or 
+    axis < -matrix.ndim)) or matrix.ndim > 2:
         return None
     # for norm type -> l1
-
     if norm_type == 'l1':
         norm = np.sum(np.abs(matrix), axis=axis, keepdims=True)
 
+    # for norm type -> l2
     elif norm_type == 'l2':
         norm = np.sqrt(np.sum(matrix**2, axis=axis, keepdims=True))
 
+     # for norm type -> max
     elif norm_type == 'max':
         norm = np.max(np.abs(matrix), axis=axis, keepdims=True)
 
     else:
         return None
-    
-    norm[norm==0] = 1
+
+    norm[norm==0] = 1 # zero division handling
+
     return matrix / norm
-    
-    
