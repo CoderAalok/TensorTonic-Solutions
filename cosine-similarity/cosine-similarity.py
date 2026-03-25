8e0a747453c036924d80da04c91aeb9d95f67c39
Aalok Yadav <yadavaalok565@gmail.com>
2026-03-25 14:42:05 +0545

    Update solution for cosine-similarity

--- cosine-similarity/cosine-similarity.py
+++ cosine-similarity/cosine-similarity.py
@@ -6,10 +6,13 @@ def cosine_similarity(a, b):
     Returns: float in [-1, 1]
     """
     # Write code here
-    dot_prod = np.dot(a,b)
-    n1 = np.linalg.norm(a)
-    n2 = np.linalg.norm(b)
-    if n1 == 0 or n2 == 0:
+    a = np.array(a)
+    b = np.array(b)
+    
+    dot_product = np.dot(a,b)
+    norm1 = np.linalg.norm(a)
+    norm2 = np.linalg.norm(b)
+    if norm1 == 0 or norm2 == 0:
         return 0
-        
-    return dot_prod/(n1*n2)
+    return dot_product/(norm1 * norm2)
+    
